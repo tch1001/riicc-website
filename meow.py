1723a4cc3ab2dfb7697f567cbc6153b3d977c018
tch1001 <tanchienhao@gmail.com>
2018-11-26 00:16:07 +0800

    added basic purchase things

--- meow.py
+++ meow.py
@@ -9,8 +9,17 @@ subpages = ['home','projects','news','members','gallery','3dprinting','purchase'
 def page_not_found(e):
 	return render_template("404.html"), 404
 
+@app.route('/')
 @app.route('/<sub>')
 def index(sub = None):
 	if(sub == None): sub = 'home'
 	print(sub)
 	return render_template('{}.html'.format(sub))
+@app.route('/purchase/submit', methods=['POST'])
+def submitPurchase():
+	form = PurchaseForm()
+	assert(request.method=='POST')
+	if(form.validate() == False):
+		flash("All fields are required.")
+		return render_template('purchase.html',form=form)
+	else: return render_template("purchase_success.html")
